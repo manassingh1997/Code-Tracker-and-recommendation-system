19f121277bbaba624e3cd2f8ee28cedca6de4de2
Manas Singh <manassinghsolanki@hotmail.com>
2025-08-04 12:05:46 +0530

    feat: model, serializer and viewset for GOAL API has been defined

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Goal(models.Model):
+    PERIOD_CHOICES = [('daily', 'Daily'), ('weekly', 'Weekly'),]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    target_count = models.IntegerField()
+    period = models.CharField(max_length=10, choices=PERIOD_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
