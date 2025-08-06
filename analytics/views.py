033553b781cd548941fcc9998472ba995a1fe8bd
Manas Singh <manassinghsolanki@hotmail.com>
2025-08-06 11:46:31 +0530

    fix: solved issues related to field name in models

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from logs.models import CodingLog
+from django.db.models.functions import TruncDate
+from django.db.models import Count 
 
 # Create your views here.
+class HeatmapView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        #Group logs by date and count them
+        logs = (
+            CodingLog.objects
+            .filter(user=user)
+            .annotate(log_day=TruncDate('date'))
+            .values('log_day')
+            .annotate(count=Count('id'))
+            .order_by('log_day')
+        )
+
+        return Response(logs)
+    
+class TopicDistributionView(APIView):
+    permisison_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        logs = (
+            CodingLog.objects
+            .filter(user=user)
+            .values('topic')
+            .values('topic')
+            .annotate(count=Count('id'))
+            .order_by('-count')
+        )
+
+        return Response(logs)
--- smart_coding_tracker/urls.py
+++ smart_coding_tracker/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
     path('api/logs/', include('logs.urls')),
     path('api/goals/', include('goals.urls')),
     #path('api/recommendations/', include('recommendations.urls')),
-    ('api/analytics/', include('analytics.urls')),
+    path('api/analytics/', include('analytics.urls')),
 ]
